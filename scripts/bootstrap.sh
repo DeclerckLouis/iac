799c6124ce67f771082e5c822fc9c2f00c9a5753
declercklouis <declerck.louis@outlook.com>
2024-07-09 16:36:50 +0200

    fix deprecation warning

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -42,7 +42,7 @@ echo ""
 # Get Docker gpg key and add repo
 # the tee command is used to overwrite the file if it already exists
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/docker-archive-keyring.gpg > /dev/null
-add-apt-repository -y "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /dev/null
+echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo "Docker repo added."
 
 # Update package list and install full Docker suite
