5ef402edf37b61b4a4fc8fb411d184f489b751df
declercklouis <declerck.louis@outlook.com>
2024-07-09 15:59:39 +0200

    added docker test

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -32,12 +32,38 @@ apt-get -y install python3-pip > /dev/null
 echo "Done."
 echo ""
 
-# Install Ansible
-echo "Installing Ansible..."
-python3 -m pip install ansible > /dev/null
-ansible_version = $(ansible --version)
-echo "Ansible version: $ansible_version"
+# Install Docker
+# Install Docker requirements
+echo "Installing Docker"
+apt-get -y install apt-transport-https ca-certificates curl software-properties-common > /dev/null
+apt-get -y install docker.io > /dev/null
+echo "Docker requirements installed."
+echo ""
+
+# Get Docker repo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - > /dev/null
+add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /dev/null
+echo "Docker repo added."
+
+# Update package list and install full Docker suite
+apt-get -y update > /dev/null
+apt-get -y install docker-* > /dev/null
+echo "Docker installed."
+echo "Done."
+echo ""
+
+# Pull Ansible Docker image
+echo "Pulling Ansible Docker image..."
+docker pull cytopia/ansible:latest
+echo "Done."
+echo ""
+
+# Run Ansible playbook using Docker
+echo "Running Ansible playbook using Docker..."
+# This command mounts the ansible directory in the current directory to the /ansible directory in the container.
+# It also mounts the k3s and kube directories to the container.
+# The playbook is then run using the bootstrap.yaml playbook and the bootstrap inventory file.
+docker run --rm -v $(pwd)/ansible:/ansible -v /etc/rancher/k3s:/etc/rancher/k3s -v ~/.kube:/root/.kube cytopia/ansible:latest ansible-playbook /ansible/playbooks/bootstrap.yaml -i /ansible/inventories/bootstrap.yaml
 echo "Done."
 
-cd ../ansible
 
