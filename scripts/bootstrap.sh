4ba2b700ebbc8bf4adde533d86bd26d3299ea987
louis.declerck <louis.declerck@dotocean.eu>
2024-07-23 15:46:40 +0200

    added todo

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -187,6 +187,7 @@ elif [ "$NODE_TYPE" = "master" ]; then
       --disable-network-policy \
       --server https://${cluster_ip}:6443
 
+# TODO: fix token usage for worker nodes
 elif [ "$NODE_TYPE" = "worker" ]; then
   curl -sfL https://get.k3s.io | K3S_AGENT_TOKEN=$k3s_token_value sh -s - agent --server https://${cluster_ip}:6443 --token $k3s_token_value
 else
