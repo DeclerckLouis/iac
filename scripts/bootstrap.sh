96184fac27656566014372fb7d0197cded540188
declercklouis <declerck.louis@outlook.com>
2024-07-29 11:08:11 +0200

    removed operator replicas since they break single node deployment

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -273,7 +273,6 @@ if [ "$NODE_TYPE" = "initmaster" ]; then
     --version 1.16.0 \
     --namespace kube-system \
     --set devices="{$INTERFACE}" \
-    --set operator.replicas=2 \
     --set envoy.enabled=true \
     --set envoy.securityContext.capabilities.keepCapNetBindService=true \
     --set envoy.securityContext.capabilities.envoyNetworkBind=true \
